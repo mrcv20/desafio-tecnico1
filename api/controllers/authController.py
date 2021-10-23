e7a51ccafce0f6f5b5ded776b1ce9871f82411d5
Maroc <marcos.valentee20@gmail.com>
2021-10-23 15:20:31 -0400

    tratando erros e criando update user

--- api/controllers/authController.py
+++ api/controllers/authController.py
@@ -1,73 +1,88 @@
 from flask import request, jsonify
 from flask.helpers import make_response
 from werkzeug.security import generate_password_hash, check_password_hash
-from .. import db
+from ..database.database import session
 from ..model.User import User, Address
 import jwt 
 from ..config import Config
+from datetime import datetime
 
 class AuthController:
     def signup_user():
         data = request.get_json()
-        hashed_password = generate_password_hash(data['password'], method='sha256')
-        print(len(data))
-        print(len(data['addresses'][0]))
-        if len(data) > 5 or len(data['addresses']) > 2:
-            return jsonify({"message": "paylod incorret, please review the format"}), 400
-        elif len(data['addresses'][0]) > 3 or len(data['addresses'][1]) > 3:
-            return jsonify({"message": "payload incorret, please review the addresses param"}), 400
-        elif len(data['password']) < 6:
-            return jsonify({"message": "Password need at least 6 characters"}), 400
-        elif len(data['firstName']) < 2:
-            return jsonify({"message": "First name too short"}), 400
-        else:
-            try:
-                new_address1 = Address(
-                    street=data['addresses'][0]['street'],
-                    number=data['addresses'][0]['number'],
-                    city=data['addresses'][0]['city']
-                )
-                new_address2 = Address(
-                    street=data['addresses'][1]['street'],
-                    number=data['addresses'][1]['number'],
-                    city=data['addresses'][1]['city']
-                )
-                db.session.add(new_address1)
-                db.session.add(new_address2)
-                db.session.commit()
-
-                new_user = User(
-                firstName=data['firstName'],
-                lastName=data['lastName'],
-                birthdate=data['birthdate'],
-                password=hashed_password,
-                id_address1=new_address1.id,
-                id_address2=new_address2.id)
-                db.session.add(new_user)
-                db.session.commit()
-            except Exception as error:
-                if type(error) == KeyError:
-                    return jsonify({"message_error": f"{error} param is required", 
-                    f"message": f"Key {error} is invalid or incorrect"}), 400
-                else:
-                    return jsonify({"message": "could not verify"}), 500
-        
-       
-        return jsonify({"message": "User registered successfully"})
+        if not data:
+            return jsonify({"message": "missing payload"})
+        try:
+            # hashed_password = generate_password_hash(data['password'], method='sha256')
+            if len(data) > 5 or len(data['addresses']) > 2:
+                return jsonify({"message": "paylod incorret, please check params"}), 400
+            elif len(data['addresses'][0]) > 3 or len(data['addresses'][1]) > 3:
+                return jsonify({"message": "payload incorret, please the the addresses param"}), 400
+            # elif len(data['password']) < 6:
+            #     return jsonify({"message": "P
+            # assword need at least 6 characters"}), 400
+            elif len(data['firstName']) < 2:
+                return jsonify({"message": "First name too short"}), 400
+        except Exception as error:
+            if type(error) == KeyError:
+                return jsonify({"message": f"param {error} is missing or incorrectw"}), 400
+            elif not data['password']:
+                return jsonify({"message": f"User {data['firstName']} registered successfully"})
+            else:
+                return jsonify({"message": "could not verify"}), 500
+        try:
+            birthdate_formated = datetime.strptime(data['birthdate'], '%Y-%m-%d')
+            new_user = User(
+            firstName=data['firstName'],
+            lastName=data['lastName'],
+            birthdate=birthdate_formated)
+            session.add(new_user)
+            session.commit()
+            new_address1 = Address(
+                street=data['addresses'][0]['street'],
+                number=data['addresses'][0]['number'],
+                city=data['addresses'][0]['city'],
+                user_id=new_user.id
+            )
+            new_address2 = Address(
+                street=data['addresses'][1]['street'],
+                number=data['addresses'][1]['number'],
+                city=data['addresses'][1]['city'],
+                user_id=new_user.id
+            )
+            session.add(new_address1)
+            session.add(new_address2)
+            session.commit()
+        except Exception as error:
+            if type(error) == KeyError:
+                if 'street' or 'number' or 'city' in str(error):
+                    return jsonify({"message": f"param {error} is missing or is incorrect, check the addresses"})
+                return jsonify({"message": f"{error} is missing or incorrect"}), 400
+            elif type(error) == ValueError:
+                if 'time data' in str(error):
+                    return jsonify({"message": f"the birthdate format is incorrect"}), 400
+            else:
+                return jsonify({"message", "Could not verify"}), 500
+    
+        token = jwt.encode({"user_id": new_user.id}, Config.SECRET_KEY, algorithm="HS256")
+        print(token)
+        return jsonify({"message": f"User {data['firstName']} registered successfully"},
+        {"token": token}), 201
 
     def login_user():
         auth = request.authorization
 
+
         if not auth or not auth.username or not auth.password:
             return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!'})
         
-        user = User.query.filter_by(firstName=auth.username).first()
+        user = session.query(User).filter_by(firstName=auth.username).first()
 
         if not user:
             return jsonify({"messsage": "User not found"})
         
         if check_password_hash(user.password, auth.password):
-            token = jwt.encode({"some": "payload"}, Config.SECRET_KEY, algorithm="HS256")
+            token = jwt.encode({"user_id": user.id}, Config.SECRET_KEY, algorithm="HS256")
             print(token)
             return jsonify({"token": token})
 
--- api/controllers/usersController.py
+++ api/controllers/usersController.py
@@ -1,47 +1,61 @@
 from re import A
 from flask import request, jsonify
-from .. import db
+from ..database.database  import session
 from api.model.User import Address, User
-from sqlalchemy.orm import aliased
-
+from datetime import datetime
 
 class UserController:
-
-    def lista_usuario():
+    def list_users():
         from api.schema.userSchema import many_users_schema
         try:
-            users = db.session.query(
-            User.firstName,
-            User.lastName,
-            User.birthdate,
-            Address.city,
-            Address.number,
-            Address.street).\
-            join(Address, (Address.id == User.id_address1)).all()
+            users = session.query(User).join(Address, (User.id == Address.user_id)).all()
+            if not users:
+                return jsonify({"message": "users not found"})
         except Exception as error:
             return jsonify({"message": f"{error}"}), 500
         return jsonify({"users": many_users_schema.dump(users)}), 200
 
-    def delete_user():
-        user_to_delete = User.query.get_or_404(id)
+    def delete_user(id):
+        user_to_delete = session.query(User).filter(User.id == id).first()
+        if not user_to_delete:
+            return jsonify({"message": "User not found"}), 400
         try:
-            db.session.delete(user_to_delete)
-            db.session.commit()
+            session.delete(user_to_delete)
+            session.commit()
+            session.close()
         except Exception as error:
             return error
         return jsonify({"message": "User deleted sucessfully"})
 
-    def update_user():
-        user_to_update = User.query.get_or_404(id)
-
+    def update_user(id):
+        user_to_update = session.query(User).filter(User.id == id).first()
+        if not user_to_update:
+            return jsonify({"message": "User not found"}), 400
         data = request.get_json()
-        user_to_update.firstName = data['firstName']
-        user_to_update.lastName = data['lastName']
-        user_to_update.birthdate = data['birthdate']
-        user_to_update.addresses = data['addresses']
+        print(len(data['birthdate']))
+        if not data:
+            return jsonify({"message": "missing payload"})
         try:
-            db.session.commit()
+            birthdate_formated = datetime.strptime(data['birthdate'], '%Y-%m-%d')
+            if len(data) > 3:
+                data = list(request.get_json())
+                return jsonify({"message": f"Operation invalid, maybe the data is incorrect"}), 400
+            user_to_update.firstName = data['firstName']
+            user_to_update.lastName = data['lastName']
+            user_to_update.birthdate = birthdate_formated
+            session.commit()
         except Exception as error:
-            return error
+            print(error)
+            if type(error) == KeyError:
+                return jsonify({"message": f"key {error} is missing or incorrect"}), 400
+            elif type(error) == ValueError:
+                if 'time data' in str(error):
+                    return jsonify({"message": f"the birthdate format is incorrect"}), 400
+                if 'unconverted data remains' in str(error):
+                    return jsonify({"message": f"could not verify the birthdate format"}), 400
+                print(error)
+                return jsonify({"message": "Could not verify"}), 500
+            
         return jsonify({"message": "User updated sucessfully"})
+
 
